3608f8900c8654118ae1a5c788bdd5916c8682d7
Matias Pulkkinen <matias.pulkkinen@hotmail.com>
2024-01-31 11:13:03 +0200

    level rendering

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
+import { renderLevel } from "./modules/level.js";
+
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+export const ctx = canvas.getContext("2d");
+
+export const tileSize = 32;
+
+let lastTime = 0;
+
+update();
+
+function update() {
+    const currentTime = new Date().getTime();
+    const deltaTime = Math.min((currentTime - lastTime), 50);
+    lastTime = currentTime;
+
+    renderLevel();
+
+    window.requestAnimationFrame(update);
+}
