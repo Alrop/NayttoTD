7db48b2f6214170ae9621a0b0fd8ea93be84d9e9
Alrop <ropoaleksi@gmail.com>
2024-02-16 10:39:26 +0200

    Tornin rakentaminen maksaa rahaa

--- modules/player.js
+++ modules/player.js
@@ -1,26 +1,37 @@
-import { ctx } from "../main.js";
+/** @format */
+
+import { ctx } from '../main.js';
 
 let gold = 100;
 let lives = 10;
 
 export function setGold(amount) {
-    if (gold + amount < 0) {
-        return false;
-    }
-    
-    gold += amount;
+	if (gold + amount < 0) {
+		return false;
+	}
+
+	gold += amount;
+}
+
+export function canAfford(cost) {
+	if (cost <= gold) {
+		gold -= cost;
+		return true;
+	} else {
+		return false;
+	}
 }
 
 export function takeDamage(damage) {
 	lives -= damage;
 	if (lives <= 0) {
-        console.log("game over");
+		console.log('game over');
 	}
 }
 
 export function drawUI() {
-    ctx.fillStyle = "white";
-    ctx.font = "16px sans-serif";
-    ctx.fillText("Lives: " + lives, 800, 24);
-    ctx.fillText("Gold: " + gold, 800, 48);
+	ctx.fillStyle = 'white';
+	ctx.font = '16px sans-serif';
+	ctx.fillText('Lives: ' + lives, 800, 24);
+	ctx.fillText('Gold: ' + gold, 800, 48);
 }
