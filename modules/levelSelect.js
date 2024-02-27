4f2f2922eff0b66ffb4f54ea1f46f59993f0c1b7
Matias Pulkkinen <matias.pulkkinen@hotmail.com>
2024-02-27 11:52:03 +0200

    Kolmas taso lisätty

--- modules/levelSelect.js
+++ modules/levelSelect.js
@@ -5,7 +5,7 @@ class LevelSelectButton {
     constructor(level, x, y) {
         this.level = level;
         this.position = { x: x, y: y };
-        this.size = { x: 96, y: 64 };
+        this.size = { x: 100, y: 64 };
     }
 
     clicked() {
@@ -21,19 +21,26 @@ class LevelSelectButton {
         ctx.fillStyle = 'white';
         ctx.font = '16px sans-serif';
         ctx.textAlign = 'center';
-        ctx.fillText("Level" + this.level, this.position.x + this.size.x / 2, this.position.y + this.size.y / 2);
+        ctx.fillText("Level " + this.level, this.position.x + this.size.x / 2, this.position.y + this.size.y / 2);
     }
 }
 
-const level1Button = new LevelSelectButton(1, 32, 32);
-const level2Button = new LevelSelectButton(2, 152, 32);
+const level1Button = new LevelSelectButton(1, 50, 80);
+const level2Button = new LevelSelectButton(2, 175, 80);
+const level3Button = new LevelSelectButton(3, 300, 80);
 
 export const levelSelectButtons = [
     level1Button,
-    level2Button
+    level2Button,
+    level3Button
 ];
 
 export function levelSelect() {
+    ctx.fillStyle = 'white';
+    ctx.font = '24px sans-serif';
+    ctx.textAlign = 'center';
+    ctx.fillText("Tower Defense", 480, 40)
+
     levelSelectButtons.forEach(button => {
         button.draw();
     });
