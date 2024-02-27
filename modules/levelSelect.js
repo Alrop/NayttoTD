a7db8d7b27dcb7029465d723b4ad8c9f5d644dc9
Matias Pulkkinen <matias.pulkkinen@hotmail.com>
2024-02-27 12:15:11 +0200

    Lisätty kuvat level selectiin

--- modules/levelSelect.js
+++ modules/levelSelect.js
@@ -2,10 +2,11 @@ import { ctx, startGame } from "../main.js";
 import { loadLevel } from "./level.js";
 
 class LevelSelectButton {
-    constructor(level, x, y) {
+    constructor(level, x, y, image) {
         this.level = level;
         this.position = { x: x, y: y };
-        this.size = { x: 100, y: 64 };
+        this.size = { x: 256, y: 225 };
+        this.image = image;
     }
 
     clicked() {
@@ -21,13 +22,22 @@ class LevelSelectButton {
         ctx.fillStyle = 'white';
         ctx.font = '16px sans-serif';
         ctx.textAlign = 'center';
-        ctx.fillText("Level " + this.level, this.position.x + this.size.x / 2, this.position.y + this.size.y / 2);
+        ctx.fillText("Level " + this.level, this.position.x + this.size.x / 2, this.position.y + 28);
+
+        ctx.drawImage(this.image, this.position.x + 32, this.position.y + 40);
     }
 }
 
-const level1Button = new LevelSelectButton(1, 50, 80);
-const level2Button = new LevelSelectButton(2, 175, 80);
-const level3Button = new LevelSelectButton(3, 300, 80);
+const level1image = new Image();
+level1image.src = '../gfx/td_level1.png';
+const level2image = new Image();
+level2image.src = '../gfx/td_level2.png';
+const level3image = new Image();
+level3image.src = '../gfx/td_level3.png';
+
+const level1Button = new LevelSelectButton(1, 50, 100, level1image);
+const level2Button = new LevelSelectButton(2, 350, 100, level2image);
+const level3Button = new LevelSelectButton(3, 650, 100, level3image);
 
 export const levelSelectButtons = [
     level1Button,
@@ -39,7 +49,7 @@ export function levelSelect() {
     ctx.fillStyle = 'white';
     ctx.font = '24px sans-serif';
     ctx.textAlign = 'center';
-    ctx.fillText("Tower Defense", 480, 40)
+    ctx.fillText("Tower Defense", 480, 50)
 
     levelSelectButtons.forEach(button => {
         button.draw();
